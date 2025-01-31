26de5dca01a8e7df7af07664da09ef0d4dce3139
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-01-31 15:38:02 +0530

    deleted extra files

--- 02 Stack Queue HashMap and Hashing Function/src/sampleproblems/hashmaps/longestconsecutivesequence/Main.java
+++
@@ -1,8 +0,0 @@
-package sampleproblems.hashmaps.longestconsecutivesequence;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {100, 4, 200, 1, 3, 2};
-        System.out.println("Longest consecutive sequence length: " + LongestConsecutiveSequence.findLongestSequence(arr));
-    }
-}
