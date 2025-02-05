9989fafb8a3ceab1c2f42418935a871b42634ec4
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-02-05 11:07:41 +0530

    [Shriyansh Kesharwani] Add . Successfully added FileReader Problem 1: Read a File Line by Line Using FileReader

---
+++ 04 and 05 Linear Search and Binary Search_StringBuilder StringBuffer BufferedReader FileReader InputStreamReader/src/filereader/readfilelinebyline/Main.java
@@ -0,0 +1,10 @@
+package filereader.readfilelinebyline;
+
+import static filereader.readfilelinebyline.ReadFileLineByLine.readFile;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "example.txt";
+        readFile(filePath);
+    }
+}
